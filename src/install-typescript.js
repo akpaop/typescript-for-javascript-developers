d54fb639c84375fca6c8fe76eed76b9057ea86b8
Noriyuki <decoy.nori@gmail.com>
2022-02-06 08:22:19 +0900

    ts-nodeをインストール

--- src/install-typescript.js
+++ src/install-typescript.js
@@ -1,3 +1,11 @@
-var message = "Hello TypeScript!";
+var message = 'Hello TypeScript!';
 console.log({ message: message });
-// ../node_modules/.bin/tsc src/install-typescript.ts
+//2022-02-05
+// ./node_modules/.bin/tsc src/install-typescript.ts
+// npx tsc src/install-typescript.ts
+// install-typescript.js
+//2022-02-06
+//バージョン調査
+// npm info ts-node
+// npm install ts-node --save-dev ts-node@10.4.0
+// npx ts-node src/install-typescript.ts
